5896b36bea048ae0e9446a2874d38f3035dd44d7
Amir Czwink <amir130@hotmail.de>
2023-10-07 21:52:40 +0200

    resolved #1

---
+++ AddOns/6G30/config.cpp
@@ -0,0 +1,10 @@
+//Only the required stuff such that bis_resistance shows as installed
+class CfgPatches
+{
+  class 6G30
+  {
+    units[]={"HeavyGrenadier"};
+    weapons[]={"6G30"};
+    requiredVersion=1.22;
+  };
+};
---
+++ AddOns/ABox/config.cpp
@@ -0,0 +1,10 @@
+//Only the required stuff such that bis_resistance shows as installed
+class CfgPatches
+{
+  class ABox
+  {
+    units[]={"AmmoBoxAddW","AmmoBoxAddE"};
+    weapons[]={};
+    requiredVersion=1.22;
+  };
+};
